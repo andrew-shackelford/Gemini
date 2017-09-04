53d45b668181895d672b490c15f559ea3b4bb6e8
Andrew Shackelford <andrewshackelford97@gmail.com>
2017-09-04 19:14:05 -0400

    added watch skeleton code

---
+++ GeminiWatch Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  GeminiWatch Extension
+//
+//  Created by Andrew Shackelford on 9/4/17.
+//  Copyright © 2017 Golden Dog Productions. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ GeminiWatch Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  GeminiWatch Extension
+//
+//  Created by Andrew Shackelford on 9/4/17.
+//  Copyright © 2017 Golden Dog Productions. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
